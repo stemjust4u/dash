a2f9c7286cc55b4bf54b8b158cf8e631e41ede5e
stemjust4u <stemjust4u@gmail.com>
2021-05-26 19:32:08 -0600

    upload new app

--- CHART-XY-HISTO_app.py
+++ CHART-XY-HISTO_app.py
@@ -15,8 +15,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('/home/chawn1272/Documents/aser-data-tall-dash2.csv')
 
-figH1 = px.histogram(df, x="question", nbins=15)
-
 fig1 = px.scatter(df, x="parm1", y="assessment",
                  size="question", color="parm2v1", hover_name="company",
                  log_x=False, size_max=25)
@@ -25,23 +23,25 @@ fig2 = px.scatter(df, x="parm1", y="question",
                  size="question", color="parm2v1", hover_name="company",
                  log_x=False, size_max=30)
 
+figH1 = px.histogram(df, x="question", nbins=15)
+
 app.layout = html.Div([
     html.Div([
     dcc.Graph(
         id='parm1 vs assessment',
-        figure=fig2
+        figure=fig1
     )
 ], style={'width': '33%', 'display': 'inline-block', 'padding': '0 20'}),
     html.Div([
     dcc.Graph(
         id='parm1 vs question',
-        figure=figH1
+        figure=fig2
     )
 ], style={'width': '33%', 'display': 'inline-block', 'padding': '0 20'}),
     html.Div([
     dcc.Graph(
         id='Question Histogram',
-        figure=fig1
+        figure=figH1
     )
 ], style={'width': '33%', 'display': 'inline-block', 'padding': '0 20'})
 ])
